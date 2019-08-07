b41516a6b8d0ae065efcaa042c9c706368be2255
Karol Nowacki <karol.nowacki@mapp.com>
2019-08-07 15:54:39 +0200

    version 0.1.1

--- Plugin/Sales/Api/OrderRepositoryInterfacePlugin.php
+++ Plugin/Sales/Api/OrderRepositoryInterfacePlugin.php
@@ -10,13 +10,43 @@ class OrderRepositoryInterfacePlugin {
 
   protected $scopeConfig;
   protected $_helper;
+  protected $productHelper;
+  protected $addressConfig;
+  protected $paymentHelper;
 
   public function __construct(
     \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-    \Mapp\Connect\Helper\Data  	$helper
+    \Mapp\Connect\Helper\Data  	$helper,
+    \Magento\Catalog\Helper\Product $productHelper,
+    \Magento\Customer\Model\Address\Config $addressConfig,
+    \Magento\Payment\Helper\Data $paymentHelper
   ) {
     $this->scopeConfig = $scopeConfig;
     $this->_helper = $helper;
+    $this->productHelper = $productHelper;
+    $this->addressConfig = $addressConfig;
+    $this->paymentHelper = $paymentHelper;
+  }
+
+  protected function getSelectedOptions($item) {
+    $options = $item->getProductOptions();
+    $options = array_merge(
+      isset($options['options']) ? $options['options'] : [],
+      isset($options['additional_options']) ? $options['additional_options'] : [],
+      isset($options['attributes_info']) ? $options['attributes_info'] : []
+    );
+    $ret = [];
+    foreach ($options as $opt)
+      $ret[] = $opt['label'].': '.$opt['value'];
+    return implode(', ', $ret);
+  }
+
+  protected function getCategories($item) {
+    $ret = [];
+    foreach ($item->getProduct()->getCategoryCollection()->addAttributeToSelect('name') as $category) {
+      $ret[]  = $category->getName();
+    }
+    return implode(', ', $ret);
   }
 
   public function afterSave(OrderRepositoryInterface $subject, OrderInterface $order): OrderInterface {
@@ -27,12 +57,29 @@ class OrderRepositoryInterfacePlugin {
 
           foreach ($order->getAllVisibleItems() as $item) {
             $item_data = $item->getData();
+            $item_data['base_image'] = $this->productHelper->getImageUrl($item->getProduct());
+            $item_data['url_path'] = $item->getProduct()->getProductUrl();
+            $item_data['categories'] = $this->getCategories($item);
+            $item_data['manufacturer'] = $item->getProduct()->getAttributeText('manufacturer');
+
+            $item_data['variant'] = $this->getSelectedOptions($item);
             unset($item_data['product_options'], $item_data['extension_attributes'], $item_data['parent_item']);
+
             $data['items'][] = $item_data;
           }
 
           $data['billingAddress'] = $order->getBillingAddress()->getData();
           $data['shippingAddress'] = $order->getShippingAddress()->getData();
+
+          $renderer = $this->addressConfig->getFormatByCode('html')->getRenderer();
+
+          $data['billingAddressFormatted'] = $renderer->renderArray($order->getBillingAddress());
+          $data['shippingAddressFormatted'] = $renderer->renderArray($order->getShippingAddress());
+
+          $data['payment_info'] = $this->paymentHelper->getInfoBlockHtml(
+             $order->getPayment(),
+             $data['store_id']
+          );
 
           if ($mc = $this->_helper->getMappConnectClient()) {
             $mc->event('transaction', $data);
